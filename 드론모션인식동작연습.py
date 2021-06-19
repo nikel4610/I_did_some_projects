09cdf50f5498ed8b224f75ab15aac4075b0e048d
Hyun - Woo <nikel4610@gmail.com>
2021-06-19 16:19:00 +0900

    Update 드론모션인식동작연습.py

--- 드론모션인식동작연습.py
+++ 드론모션인식동작연습.py
@@ -4,10 +4,9 @@ import numpy as np
 
 max_num_hands = 2
 gesture = {
-    0:'fist', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
-    6:'six', 7:'rock', 8:'spiderman', 9:'yeah', 10:'ok',
-} #기본 설정되어있는 제스쳐 손동작
-rps_gesture = {0:'rock', 5:'paper', 9:'scissors'} # <- 임시 수정 제스쳐
+    0:'zero', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+    6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten',
+}
 
 # MediaPipe hands model
 mp_hands = mp.solutions.hands
@@ -18,13 +17,13 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.5)
 
 # Gesture recognition model
-file = np.genfromtxt('data/gesture_train.csv', delimiter=',') #손동작 csv파일
+file = np.genfromtxt('data/gesture_train.csv', delimiter=',')
 angle = file[:,:-1].astype(np.float32)
 label = file[:, -1].astype(np.float32)
 knn = cv2.ml.KNearest_create()
 knn.train(angle, cv2.ml.ROW_SAMPLE, label)
 
-cap = cv2.VideoCapture('http://172.30.1.37:4747/video')
+cap = cv2.VideoCapture('http://192.168.35.43:4747/video')
 
 while cap.isOpened():
     ret, img = cap.read()
@@ -66,43 +65,17 @@ while cap.isOpened():
             idx = int(results[0][0])
 
             # Draw gesture result
-            if idx in rps_gesture.keys():
+            if idx in gesture.keys():
                 org = (int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0]))
-                cv2.putText(img, text=rps_gesture[idx].upper(), org=(org[0], org[1] + 20),
-                            fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                            color=(255, 255, 255), thickness=2)
+                cv2.putText(img, text=gesture[idx].upper(), org=(org[0], org[1] + 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
                 rps_result.append({
-                    'rps': rps_gesture[idx],
+                    'rps': gesture[idx],
                     'org': org
                 })
 
             mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
 
-            # 이부분 드론 조종으로 수정
-            if len(rps_result) >= 2:
-                winner = None
-                text = ''
-
-                if rps_result[0]['rps']=='rock':
-                    if rps_result[1]['rps']=='rock'     : text = 'Tie'
-                    elif rps_result[1]['rps']=='paper'  : text = 'Paper wins'  ; winner = 1
-                    elif rps_result[1]['rps']=='scissors': text = 'Rock wins'   ; winner = 0
-                elif rps_result[0]['rps']=='paper':
-                    if rps_result[1]['rps']=='rock'     : text = 'Paper wins'  ; winner = 0
-                    elif rps_result[1]['rps']=='paper'  : text = 'Tie'
-                    elif rps_result[1]['rps']=='scissors': text = 'Scissors wins'; winner = 1
-                elif rps_result[0]['rps']=='scissors':
-                    if rps_result[1]['rps']=='rock'     : text = 'Rock wins'   ; winner = 1
-                    elif rps_result[1]['rps']=='paper'  : text = 'Scissors wins'; winner = 0
-                    elif rps_result[1]['rps']=='scissors': text = 'Tie'
-
-                if winner is not None:
-                    cv2.putText(img, text='Winner', org=(rps_result[winner]['org'][0], rps_result[winner]['org'][1] + 70),
-                                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(0, 255, 0), thickness=3)
-                cv2.putText(img, text=text, org=(int(img.shape[1] / 2), 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                            fontScale=2, color=(0, 0, 255), thickness=3)
-
-    cv2.imshow('Game', img)
+    cv2.imshow('Camtest', img)
     if cv2.waitKey(1) == ord('q'):
         break
